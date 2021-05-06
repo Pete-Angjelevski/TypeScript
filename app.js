960db1c4252167558c24fc0e83b6bb702b1565da
Pete Angjelevski <p.angjelevski@gmail.com>
2021-05-06 18:14:12 +1200

    function types

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ function add(n1, n2) {
 function printResult(num) {
     console.log('result: ' + num);
 }
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
 printResult(add(5, 12));
-var combineValues;
-combineValues = add;
+var combineValues; // we are defining that expected variable will be defined in the future with function with 2 number parameters that returns number
+combineValues = add; // if not defined during let then future reassignment of variable will carry 'any' type
+// combineValues = printResult                        complains as not defined as per above
 console.log(combineValues(10, 10));
+addAndHandle(10, 10, function (result) {
+    console.log(result);
+});
---
+++ functions.ts
@@ -0,0 +1,33 @@
+function add (n1: number, n2: number): number  {      // can infer expected return type by defining after parameters
+  return n1 + n2
+}
+
+
+function printResult (num: number): void {            // console.log() has return type void - void means does not have return 
+  console.log('result: ' + num)
+}
+
+
+function addAndHandle (n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2
+  cb(result)
+}
+
+
+printResult(add(5,12))
+
+
+let combineValues: (a: number, b: number) => number   // we are defining that expected variable will be defined in the future with function with 2 number parameters that returns number
+
+combineValues = add                                   // if not defined during let then future reassignment of variable will carry 'any' type
+
+// combineValues = printResult                        complains as not defined as per above
+
+
+console.log(combineValues(10, 10))
+
+
+
+addAndHandle(10, 10, (result) => {
+  console.log(result)
+})
