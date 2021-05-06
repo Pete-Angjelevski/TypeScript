ce18ea10bd4c5aa645faf145cb3835343f5f1b47
Pete Angjelevski <p.angjelevski@gmail.com>
2021-05-06 16:52:38 +1200

    define type enum

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 // const person: {
 //   name: string,
 //   age: number
-// }  = {              // defines object properties now 
+// }             // defines object properties now 
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: "Pete",
     age: 30,
-    hobbies: ["MMA", "Movies"]
+    hobbies: ["MMA", "Movies"],
+    role: [2, 'author'],
+    role2: Role.ADMIN
 };
+person.role.push('admin'); // push is an exception to tuples 
 var favouriteActivities;
 favouriteActivities = ['Sport'];
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log('Hobbies: ', hobby);
+    // console.log(hobby.map()) // pre-emptively shows unabilty to map as there are no arrays to map over
 }
