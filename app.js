510449f573ef6d443ada012b70ed232d33679ca4
Pete Angjelevski <p.angjelevski@gmail.com>
2021-05-06 15:20:29 +1200

    ts primitive value conversion

---
+++ app.js
@@ -0,0 +1,6 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+var num1 = '5';
+var num2 = 10;
+console.log(add(num1, num2));
---
+++ app.ts
@@ -0,0 +1,8 @@
+function add(n1, n2) {
+  return n1 + n2
+}
+
+const num1 = 4
+const num2 = 10
+
+console.log(add(num1, num2))
