4c49cdf3df30d1affee21e475aae4a2cb93d29ab
Pete Angjelevski <p.angjelevski@gmail.com>
2021-05-06 15:42:20 +1200

    add ts boolean

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, n2, showResult) {
+    showResult ? console.log('hello') : n1 + n2;
 }
 var num1 = 5;
-var num2 = 10;
-console.log(add(num1, num2));
+var num2 = 5;
+var printResult = true;
+console.log(add(num1, num2, printResult));
