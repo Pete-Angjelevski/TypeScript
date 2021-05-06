72fe9d4a948b771e8684844df20c1bd69e611c01
Pete Angjelevski <p.angjelevski@gmail.com>
2021-05-06 17:46:15 +1200

    literal types & combined union variables

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
+    // if (resultConversion === 'as-number') {
+    //   return Number(result)
+    // } else {
+    //   return result.toString()
+    // }
 }
-console.log(combine('pete', 'claudia'));
-console.log(combine(10, 10));
+console.log(combine('pete', 'claudia', 'as-text'));
+console.log(combine(10, 1, 'as-number'));
+console.log(combine('10', '10', 'as-number'));
--- app.ts
+++ app.ts
@@ -1,12 +1,31 @@
-function combine(input1: number | string , input2: number | string) {     // union types 
+type StrNum = string | number     // declare global types union variable for simplicity and multiple use 
+
+
+
+
+function combine(
+  input1: number | string,                              // union types 
+  input2: StrNum,                                       // use of combined union variable  
+  resultConversion: 'as-number' | 'as-text') {          // union  + literal types 
     let result
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-      result = input1 + input2
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {     // if i type resultConverstion === 'as-num' will provide error
+      result = +input1 + +input2
     } else {
       result = input1.toString() + input2.toString()
     } 
-  return result 
+
+    return result
+
+    // if (resultConversion === 'as-number') {
+    //   return Number(result)
+    // } else {
+    //   return result.toString()
+    // }
+  
   }
   
-  console.log(combine('pete', 'claudia'))
-  console.log(combine(10, 10))
+  console.log(combine('pete', 'claudia', 'as-text'))
+
+  console.log(combine(10, 1, 'as-number'))
+
+  console.log(combine('10', '10', 'as-number'))
